901b227c99c326c890d22dbf46253d279a5600eb
Andrew B <ajbacc@gmail.com>
2020-09-18 10:25:16 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>	
+	<form action="./verifyaddr.php" method="post">
+		<label for="fname">First Name:</label><br>
+		<input type="text" id="fname" name="fname"><br>
+		<label for="lname">Last name:</label><br>
+		<input type="text" id="lname" name="lname"><br>
+
+		<label for="street1">Street1:</label><br>
+		<input type="text" id="street1" name="street1"><br>
+		<label for="street2">Street2:</label><br>
+		<input type="text" id="street2" name="street2"><br>
+		<label for="city">City:</label><br>
+		<input type="text" id="city" name="city"><br>
+		<label for="state">State:</label><br>
+		<input type="text" id="state" name="state"><br>
+		<label for="zip">ZIP code:</label><br>
+		<input type="text" id="zip" name="zip"><br>
+		<label for="country">country:</label><br>
+		<input type="text" id="country" name="country"><br>
+		<input type="submit" value="Submit">
+	</form> 	
+</html>
